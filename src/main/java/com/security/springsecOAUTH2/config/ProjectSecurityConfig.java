73122135a86e88ef7779eeb29d52c9086aa86a73
Rito Sarkar <ritosarkar79@gmail.com>
2026-02-21 09:26:45 +0530

    adding ClientRegistrationRepository related changes

--- src/main/java/com/security/springsecOAUTH2/config/ProjectSecurityConfig.java
+++ src/main/java/com/security/springsecOAUTH2/config/ProjectSecurityConfig.java
@@ -1,4 +1,47 @@
 package com.security.springsecOAUTH2.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((req) ->
+                        req.requestMatchers("/secure").authenticated()
+                                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration gitHub= githubClientRegistration();
+        ClientRegistration facebook= facebookClientRegistration();
+        return new InMemoryClientRegistrationRepository(gitHub,facebook);
+    }
+
+    private ClientRegistration githubClientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId("Ov23lir4IsDtKSe3BWJp")
+                .clientSecret("34cfc63cd7ecc27bddc6a5b60187031e63558ee7")
+                .build();
+    }
+
+    private ClientRegistration facebookClientRegistration() {
+        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook")
+                .clientId("806574325748694")
+                .clientSecret("ffe897e3c54a6c3cb1410851659a31fc")
+                .build();
+    }
 }
--- src/main/java/com/security/springsecOAUTH2/controller/SecureController.java
+++ src/main/java/com/security/springsecOAUTH2/controller/SecureController.java
@@ -1,12 +1,21 @@
 package com.security.springsecOAUTH2.controller;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SecureController {
     @GetMapping("/secure")
-    public String securePage(){
+    public String securePage(Authentication authentication){
+        if(authentication instanceof UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken){
+            System.out.println(usernamePasswordAuthenticationToken);
+        } else if (authentication instanceof OAuth2AuthenticationToken oauth2AuthenticationToken) {
+            System.out.println(oauth2AuthenticationToken);
+            
+        }
         return "secure.html";
     }
 }
