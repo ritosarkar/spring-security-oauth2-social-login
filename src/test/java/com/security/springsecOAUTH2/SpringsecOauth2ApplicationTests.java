4e7f934b78a0913342589585dd694920a11eb2ab
Rito Sarkar <ritosarkar79@gmail.com>
2026-02-21 05:38:29 +0530

    intial project commit

---
+++ src/test/java/com/security/springsecOAUTH2/SpringsecOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.springsecOAUTH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
